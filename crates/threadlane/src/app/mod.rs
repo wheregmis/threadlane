b14c48915dca85366d35659c9306c2f36d21e534
Sabin Regmi <get2sabin@gmail.com>
2026-07-23 21:52:45 -0400

    common widget

--- crates/threadlane/src/app/mod.rs
+++ crates/threadlane/src/app/mod.rs
@@ -380,6 +380,36 @@ script_mod! {
     // -------------------------------------------------------------------
     // Sessions sidebar: project folders + session rows
     // -------------------------------------------------------------------
+    let SidebarComposeButton = Button {
+        width: 24
+        height: 24
+        visible: false
+        margin: 0
+        padding: 0
+        spacing: 0
+        text: ""
+        align: Align{x: 0.5 y: 0.5}
+        icon_walk: Walk{width: 12 height: 12 margin: 0}
+        draw_icon +: {
+            svg: crate_resource("self:resources/icons/compose.svg")
+            color: #x758294
+            color_hover: #xb8d5f5
+            color_down: #xffffff
+        }
+        draw_bg +: {
+            color: #x00000000
+            color_hover: #x283544
+            color_focus: #x283544
+            color_down: #x30445b
+            border_color: #x00000000
+            border_color_hover: #x00000000
+            border_color_focus: #x00000000
+            border_color_down: #x00000000
+            border_size: 0.0
+            border_radius: 6.0
+        }
+    }
+
     let ProjectHeaderBase = RoundedView {
         width: Fill
         height: 34
@@ -451,35 +481,7 @@ script_mod! {
                 border_radius: 6.0
             }
         }
-        new_project_session_btn := Button {
-            width: 22
-            height: 22
-            visible: false
-            margin: 0
-            padding: 0
-            spacing: 0
-            text: ""
-            align: Align{x: 0.5 y: 0.5}
-            icon_walk: Walk{width: 11 height: 11}
-            draw_icon +: {
-                svg: crate_resource("self:resources/icons/plus.svg")
-                color: #x758294
-                color_hover: #xb8d5f5
-                color_down: #xffffff
-            }
-            draw_bg +: {
-                color: #x00000000
-                color_hover: #x283544
-                color_focus: #x283544
-                color_down: #x30445b
-                border_color: #x00000000
-                border_color_hover: #x00000000
-                border_color_focus: #x00000000
-                border_color_down: #x00000000
-                border_size: 0.0
-                border_radius: 6.0
-            }
-        }
+        new_project_session_btn := SidebarComposeButton {}
     }
 
     let SessionList = #(SessionList::register_widget(vm)) {
@@ -679,34 +681,7 @@ script_mod! {
                                         text_style: theme.font_bold { font_size: 8.5 }
                                     }
                                 }
-                                add_project_btn := Button {
-                                    width: 24
-                                    height: 24
-                                    visible: false
-                                    text: ""
-                                    padding: 0
-                                    spacing: 0
-                                    align: Align{x: 0.5 y: 0.5}
-                                    icon_walk: Walk{width: 10 height: 10}
-                                    draw_icon +: {
-                                        svg: crate_resource("self:resources/icons/plus.svg")
-                                        color: #x9fc3ef
-                                        color_hover: #xd3e8ff
-                                    }
-                                    draw_bg +: {
-                                        color: #x232a33
-                                        color_hover: #x2a3542
-                                        color_focus: #x2a3542
-                                        color_down: #x314257
-                                        border_color: #x384452
-                                        border_color_hover: #x526a84
-                                        border_color_focus: #x526a84
-                                        border_color_down: #x6fa8ff
-                                        border_size: 1.0
-                                        border_radius: 7.0
-                                    }
-
-                                }
+                                add_project_btn := SidebarComposeButton {}
                             }
                             session_context_menu := SessionContextMenu {}
                             session_list := SessionList { height: Fill }
